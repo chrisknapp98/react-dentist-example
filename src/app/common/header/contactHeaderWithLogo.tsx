71a3be70846fedce52a403c03969c4497b14c943
Christopher Knapp <chris.knapp@live.de>
2025-07-04 17:00:36 +0200

    link to homepage on logo click

--- src/app/common/header/contactHeaderWithLogo.tsx
+++ src/app/common/header/contactHeaderWithLogo.tsx
@@ -1,5 +1,6 @@
 import LogoPraxis from "@/images/logo_praxis.svg";
 import { ContactHeaderWithAutoAlignment } from "./contactHeaderWithAutoAlignment";
+import Link from "next/link";
 
 export function ContactHeaderWithLogo() {
     return (
@@ -8,7 +9,9 @@ export function ContactHeaderWithLogo() {
             <div className="flex justify-center xl:justify-start pb-10 lg:px-20">
                 {/* <div className="absolute top-[120px] md:top-[52px] w-[350px] sm:w-[450px] md:w-[600px] h-[113px] sm:h-[140px] md:h-[180px] z-20"> */}
                 <div className="relative xl:top-[-54px] w-[350px] sm:w-[450px] md:w-[600px] h-[105px] sm:h-[135px] md:h-[180px] z-20">
-                    <LogoPraxis className="w-full h-full rounded-b-2xl" />
+                    <Link href="/">
+                        <LogoPraxis className="w-full h-full rounded-b-2xl" />
+                    </Link>
                 </div>
             </div>
         </div>
