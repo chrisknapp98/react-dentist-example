35b1cf5ec6ece52ddcd9f539308ce9cbf11d7cac
Christopher Knapp <chris.knapp@live.de>
2025-07-09 15:09:05 +0200

    set noreferrer on instagram link and open in new tab

--- src/app/common/header/contactHeaderWithAutoAlignment.tsx
+++ src/app/common/header/contactHeaderWithAutoAlignment.tsx
@@ -13,7 +13,7 @@ export function ContactHeaderWithAutoAlignment() {
         <MailIcon className="w-5 h-5 mr-2 text-practiceWhite" />
         <a href="mailto:praxis.dumbach@outlook.de" className="underline">praxis.dumbach@outlook.de</a>
       </div>
-      <a href="https://www.instagram.com/" className="hidden md:flex items-center">
+      <a href="https://www.instagram.com/" target="_blank" rel="noreferrer" className="hidden md:flex items-center">
         <InstagramIcon className="w-5 h-5 text-practiceWhite" />
       </a>
     </div>
