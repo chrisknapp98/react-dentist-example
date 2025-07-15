25c4760284291edee26abe2a7665847b35bb01d5
Christopher Knapp <chris.knapp@live.de>
2025-07-15 18:23:30 +0200

    Revert "try changing status bar color on iOS"
    This reverts commit 28245e35c1289cf6c35fbef794e2af30a97c7c70.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,22 @@ import { WelcomeBanner } from "./comingSoon/welcomeBanner";
 import { OpeningHoursAndRecentNews } from "./comingSoon/openingHoursAndRecentNews";
 import Image from "next/image";
 import { Footer } from "./common/footer/footer";
-import Head from "next/head";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="theme-color" content="#9b2c4e" />
-      </Head>
-
-      <div className="font-sans">
-        <WelcomeBanner />
-        <OpeningHoursAndRecentNews />
-        <div className="relative w-full h-48 md:h-96 z-0">
-          <Image
-              src="/images/praxis_foto.png"
-              alt="Willkommen in unserer Zahnarztpraxis"
-              fill
-              className="object-cover w-full"
+    <div className="font-sans">
+      <WelcomeBanner />
+      <OpeningHoursAndRecentNews />
+      <div className="relative w-full h-48 md:h-96 z-0">
+        <Image
+            src="/images/praxis_foto.png"
+            alt="Willkommen in unserer Zahnarztpraxis"
+            fill
+            className="object-cover w-full"
           />
-        </div>
-        <ContactSection />
-        <Footer />
       </div>
-    </>
-  );
+      <ContactSection />
+      <Footer />
+    </div>
+  )
 }
