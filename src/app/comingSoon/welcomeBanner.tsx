9f66a8b819bff3c438d105d4503f2a1169ed80f4
Christopher Knapp <chris.knapp@live.de>
2025-10-10 23:48:26 +0200

    remove coming soon and adjust spaces

--- src/app/comingSoon/welcomeBanner.tsx
+++ src/app/comingSoon/welcomeBanner.tsx
@@ -5,7 +5,7 @@ import { ContactInformation } from "../common/contactInformation/contactInformat
 export function WelcomeBanner() {
   return (
     <section >
-      <div className="flex flex-col w-full bg-backgroundGray shadow-md text-text-grey pt-64 sm:pt-72 md:pt-[350px] pb-10 px-6 lg:px-32 xl:px-56 justify-start gap-6 md:gap-10 items-center">
+      <div className="flex flex-col w-full bg-backgroundGray shadow-md text-text-grey pt-52 sm:pt-64 md:pt-[320px] pb-10 px-6 lg:px-32 xl:px-56 justify-start gap-6 md:gap-10 items-center">
         <h1 className="text-3xl md:text-5xl font-thin text-white text-center">HERZLICH WILLKOMMEN</h1>
         <div className="text-lg text-white text-center">
         <p>
@@ -37,9 +37,9 @@ export function WelcomeBanner() {
       {/* <div className="fixed top-0 left-0 w-full z-50"> */}
         <div>
             <ContactHeader alignment="center" />
-            <div className="flex justify-center pt-4 pb-10 md:pb-20 bg-gray-200 font-light text-4xl shadow-lg">
-                <p>COMING SOON!</p>
-                <div className="absolute top-[120px] md:top-[130px] w-[300px] vs:w-[350px] sm:w-[450px] md:w-[600px] h-[90px] vs:h-[105px] sm:h-[135px] md:h-[180px] z-20">
+            <div className="flex justify-center pt-2 pb-10 md:pb-20 bg-gray-200 font-light text-4xl shadow-lg">
+                {/* <p>COMING SOON!</p> */}
+                <div className="absolute top-[70px] md:top-[90px] w-[300px] vs:w-[350px] sm:w-[450px] md:w-[600px] h-[90px] vs:h-[105px] sm:h-[135px] md:h-[180px] z-20">
                   <LogoPraxis className="w-full h-full rounded-2xl" />
                 </div>
             </div>
